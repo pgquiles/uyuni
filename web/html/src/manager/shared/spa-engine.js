bcb98510e0f00a522754ef97d06f4772155671ed
Luis Neves <luneves@suse.de>
2019-05-20 16:33:33 +0100

    Add first configuration to use Senna and transform UI into a SPA

---
+++ web/html/src/manager/shared/spa-engine.js
@@ -0,0 +1,17 @@
+import App, {HtmlScreen} from "senna";
+import "senna/build/senna.css"
+import "./spa-engine.css"
+
+var appInstance = new App();
+appInstance.setLinkSelector("a.js-spa");
+appInstance.setFormSelector("");
+appInstance.addSurfaces('page-body');
+// app.addSurfaces('breadcrumb');
+appInstance.addRoutes([{
+  path: /.*/,
+  handler: function (route, a, b) {
+    const screen = new HtmlScreen();
+    screen.setCacheable(false);
+    return screen;
+  }
+}]);
